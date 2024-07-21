120f6baa3cd0f0da14d8105ea0dc33aaf24a4fb2
portfolio-team <yamamoto.kotaro.portfolio@gmail.com>
2024-07-21 12:06:45 +0900

    add: health_check path

--- fastapi/api/main.py
+++ fastapi/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from api.routers import task, done
+from api.routers import task, done, health_check
 
 app = FastAPI()
+app.include_router(health_check.router)
 app.include_router(task.router)
 app.include_router(done.router)
---
+++ fastapi/api/routers/health_check.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/health_check")
+async def health_check():
+    return {"message": "Health Check OK!!"}
